498a9ff06ea8f147c6593c4aa51cbf5e3c0dc3d0
Maurizio Lupo <maurizio.lupo@gmail.com>
2016-02-28 04:48:25 +0000

    adding key to dedupe log

--- src/dedupe.js
+++ src/dedupe.js
@@ -19,7 +19,7 @@ function dedupeDecorator(wrapper, delay, getKey, getlogger) {
 
       function runQueue(cacheKey, err, dep) {
         var len = cacheKey in callback_queues ? callback_queues[cacheKey].length : 0;
-        logger('deduping', {len: len});
+        logger('deduping', {len: len, key: cacheKey});
         for (var i = 0; i < len; i++) {
           callback_queues[cacheKey][i](err, dep);
         }
