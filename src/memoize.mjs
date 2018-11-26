9d2368b5db94479ba4b939724409ff38c5e96264
sithmel <maurizio.lupo@gmail.com>
2018-11-26 15:20:00 +0000

    fix to memoize decorator

--- src/memoize.mjs
+++ src/memoize.mjs
@@ -1,10 +1,13 @@
 import objId from './utils/obj-id'
 import LRUCache from 'little-ds-toolkit/lib/lru-cache'
+import funcRenamer from './utils/func-renamer'
 
-export default function cacheDependency ({ len, ttl }) {
-  return function _cacheDependency (func) {
+export default function getMemoizeDecorator (opts = {}) {
+  const { len, ttl } = opts
+  return function memoize (func) {
+    const renamer = funcRenamer(`memoize(${func.name || 'anonymous'})`)
     const cache = new LRUCache({ maxLen: len, defaultTTL: ttl })
-    return function cachedService (...deps) {
+    return renamer(function (...deps) {
       const key = objId.getIdFromValues(deps)
       if (cache.has(key)) {
         return cache.get(key)
@@ -12,6 +15,6 @@ export default function cacheDependency ({ len, ttl }) {
       const result = func(...deps)
       cache.set(key, result)
       return result
-    }
+    })
   }
 }
